b52889b1b03f5cb008ff2ff13fbc76a3e842c057
Hugo Ramos Montesinos <hugodarken7@gmail.com>
2023-11-21 21:28:45 +0100

    Menu Opcions (funcionalitat finalitzada)

---
+++ Assets/Scripts/OptionsVolum.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsVolum : MonoBehaviour
+{
+    public Slider slider;
+    public float sliderVal;
+    public Image imgMute;
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("volum", 0.5f);
+        AudioListener.volume = slider.value;
+        Mute();
+    }
+    public void ChangeSlider(float valor)
+    {
+        sliderVal = valor;
+        PlayerPrefs.SetFloat("volum", sliderVal);
+        PlayerPrefs.Save();
+        AudioListener.volume = slider.value;
+        Mute();
+    }
+    public void Mute()
+    {
+        if (sliderVal == 0)
+        {
+            imgMute.enabled = true;
+        }
+        else
+        {
+            imgMute.enabled = false;
+        }
+    }
+}
