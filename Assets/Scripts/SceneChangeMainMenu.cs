26662d7d8281f3a2441457fd13562fb06fcf0f27
Hugo Ramos Montesinos <hugodarken7@gmail.com>
2024-01-31 20:52:51 +0100

    Canvi-Scene2

--- Assets/Scripts/SceneChangeMainMenu.cs
+++ Assets/Scripts/SceneChangeMainMenu.cs
@@ -10,16 +10,17 @@ public class SceneChangeMainMenu : MonoBehaviour
     [SerializeField] string to;
     [SerializeField] string additionalScene;
     [SerializeField] Button button;
-
     void Start()
     {
         Button btn = button.GetComponent<Button>();
         btn.onClick.AddListener(SwitchScene);
     }
-
     public void SwitchScene()
     {
         SceneManager.LoadScene(to);
-        SceneManager.LoadScene(additionalScene, LoadSceneMode.Additive);
+        if (!string.IsNullOrEmpty(additionalScene))
+        {
+            SceneManager.LoadScene(additionalScene, LoadSceneMode.Additive);
+        }
     }
 }
