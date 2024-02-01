32fc1b409343e77f1d4cbda7fa32090d27df081d
21alexrodri <21alexrodri@gmail.com>
2024-02-01 20:04:16 +0100

    Cambios Menus

--- Assets/Scripts/OptionsBrightness.cs
+++ Assets/Scripts/OptionsBrightness.cs
@@ -6,18 +6,22 @@ using UnityEngine.UI;
 public class OptionsBrightness : MonoBehaviour
 {
     public Slider slider;
-    public float sliderVal;
     public Image panelBrillo;
     void Start()
     {
         slider.value = PlayerPrefs.GetFloat("luminositat", 0.5f);
-        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
+        //panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
+        UpdateBrightness(1-slider.value);
     }
     public void ChangeSlider(float valor)
     {
-        sliderVal = valor;
-        PlayerPrefs.SetFloat("luminositat", sliderVal);
+        
+        PlayerPrefs.SetFloat("luminositat", valor);
         PlayerPrefs.Save();
-        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
+        UpdateBrightness(1 - valor);
+    }
+    private void UpdateBrightness(float value) {
+        float brightness = Mathf.Lerp(0.1f,0.8f,value);
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b,brightness);
     }
 }
