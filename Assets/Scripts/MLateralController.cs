ed479570d0bf9a7e61c0151397d91238161f1046
21alexrodri <21alexrodri@gmail.com>
2024-02-05 20:35:15 +0100

    ArregloBugMenuLateral

--- Assets/Scripts/MLateralController.cs
+++ Assets/Scripts/MLateralController.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MLateralController : MonoBehaviour
 {
     [SerializeField] GameObject mLateralPanel;
+    [SerializeField] GameObject menuOpcions;
+    bool comparador = false;
+    string[] escenasConMenu = {"SotanoScene", "EsencialScene" };
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            mLateralPanel.SetActive(!mLateralPanel.activeInHierarchy);
+
+        while(comparador == false) {
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                foreach (string escena in escenasConMenu)
+                {
+                    if (SceneManager.GetActiveScene().name == escena)
+                    {
+                        mLateralPanel.SetActive(!mLateralPanel.activeInHierarchy);
+                        break;
+                    }
+                }
+            }
+
         }
+        if (menuOpcions.activeInHierarchy){
+            comparador = true;
+        }
+        
     }
 }
