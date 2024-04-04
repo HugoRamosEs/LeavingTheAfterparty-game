d81825ec2eba8d2c9ff6232f9f187c3476a4e6e9
Hugo Ramos Montesinos <hugodarken7@gmail.com>
2024-04-04 20:09:29 +0200

    Menus-Semiacabados
    Faltan detalles

--- Assets/Scripts/Menus/Other/MLateralController.cs
+++ Assets/Scripts/Menus/Other/MLateralController.cs
@@ -1,31 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MLateralController : MonoBehaviour
 {
     [SerializeField] GameObject mLateralPanel;
+    [SerializeField] GameObject img;
     [SerializeField] GameObject menuOpcions;
-    bool comparador = false;
+    [SerializeField] Button btnContinuar;
     string[] escenasConMenu = { "SotanoScene", "EsencialScene" };
+
+    void Start()
+    {
+        btnContinuar.onClick.AddListener(OnContinueButtonClicked);
+    }
+
+    void OnContinueButtonClicked()
+    {
+        Time.timeScale = 1f;
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && !menuOpcions.activeInHierarchy)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             foreach (string escena in escenasConMenu)
             {
                 if (SceneManager.GetActiveScene().name == escena)
                 {
+                    img.SetActive(!img.activeInHierarchy);
                     mLateralPanel.SetActive(!mLateralPanel.activeInHierarchy);
                     Time.timeScale = mLateralPanel.activeInHierarchy ? 0f : 1f;
                     break;
                 }
             }
         }
-
-        comparador = menuOpcions.activeInHierarchy;
     }
 }
 
