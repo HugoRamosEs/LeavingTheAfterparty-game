649b23dce281cd5cf89f8eceb3b616acd06bcc43
hMonroyy <hector.monroy.delahoz@gmail.com>
2024-05-21 22:25:52 +0200

    bug-cargar-partida

--- Assets/Scripts/Web/CargarPartida.cs
+++ Assets/Scripts/Web/CargarPartida.cs
@@ -20,28 +20,19 @@ public class CargarPartida : MonoBehaviour
 
     IEnumerator CargarDatos()
     {
-        Debug.Log("1C");
-        cargando = false;
         cargando = true;
-        Debug.Log("2C");
         //imLoading.SetActive(true);
         yield return new WaitForSeconds(0.5f);
-        Debug.Log("3C");
         string[] datos = new string[1];
         datos[0] = UserGameInfo.Instance.email;
-        Debug.Log("4C");
         StartCoroutine(servidor.ConsumirServicio("cargar-partida", datos, PosCargar));
-        Debug.Log("5C");
         //imLoading.SetActive(false);
-        Debug.Log("6C");
         yield return new WaitUntil(() => !servidor.ocupado);
         yield return new WaitWhile(() => cargando);
-        Debug.Log("7C");
     }
 
     void PosCargar()
     {
-        Debug.Log("8PC");
         mensajeText.text = servidor.respuesta.mensaje;
 
         switch (servidor.respuesta.codigo)
@@ -89,10 +80,8 @@ public class CargarPartida : MonoBehaviour
         cargando = false;
         Debug.Log("Respuesta en ProcesarRespuesa: " + respuesta);
 
-        // Dividir la respuesta en partes usando "&&" como delimitador
         string[] pares = respuesta.Split(new string[] { "&&" }, StringSplitOptions.RemoveEmptyEntries);
 
-        // Variables para almacenar los datos
         string id = "";
         string escena = "";
         float posX = 0f;
@@ -109,7 +98,6 @@ public class CargarPartida : MonoBehaviour
         bool luzSotanoEncendida = false;
         bool donutDesbloqueado = false;
 
-        // Recorrer cada par y asignar los valores a las variables
         foreach (string par in pares)
         {
             string[] partes = par.Split(':');
@@ -175,42 +163,35 @@ public class CargarPartida : MonoBehaviour
         Debug.Log("Vida: " + currentHp.ToString());
         Debug.Log("Estamina: " + currentStamina.ToString());
 
-        // Actualizar la información del juego
         UserGameInfo.Instance.UpdateGameInfo(id, escena, posX.ToString(), posY.ToString(), posZ.ToString(), currentHp.ToString("F2"), currentStamina.ToString("F2"),
             orderInLayer.ToString(), sotanoPasado.ToString(), congeladorPasado.ToString(), playaPasada.ToString(), barcoBossPasado.ToString(), ciudadBossPasado.ToString(),
             luzSotanoEncendida.ToString(), donutDesbloqueado.ToString());
 
-        // Cargar las escenas
         CargarEscenas(escena);
     }
 
     bool ConvertToBool(string value)
     {
-        // Intenta convertir la cadena a un valor booleano
         if (bool.TryParse(value, out bool result))
         {
             return result;
         }
         else
         {
-            // Si la conversión falla, intenta convertir desde un entero
             if (int.TryParse(value, out int intResult))
             {
                 return intResult != 0;
             }
         }
 
-        // Si todo lo demás falla, devuelve false o maneja el error como prefieras
         Debug.LogError($"No se pudo convertir '{value}' a un valor booleano.");
         return false;
     }
 
     float stringToFloat(string posicionDecimal)
     {
-        // Reemplazar el punto por una coma
         posicionDecimal = posicionDecimal.Replace('.', ',');
 
-        // Convertir a número flotante
         if (float.TryParse(posicionDecimal, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.GetCultureInfo("es-ES"), out float valorFlotante))
         {
             return valorFlotante;
