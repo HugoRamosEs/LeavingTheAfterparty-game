1da853bd731e50ac4e55eb3a69a787524a24bd17
Hugo Ramos Montesinos <hugodarken7@gmail.com>
2024-09-27 20:37:01 +0200

    Update php files

--- Assets/Scripts/Web/PHP/login.php
+++ Assets/Scripts/Web/PHP/login.php
@@ -3,6 +3,10 @@
 // error_reporting(E_ALL);
 // ini_set("display_errors", 1);
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 include "class/config/autoloader.php";
 spl_autoload_register("Autoloader::load");
 spl_autoload_register("Autoloader::loadDataBase");
